ef5c884ffce10c1e1409001d8f5e1537e9a7ec37
Marzieh Tajik <marzieh.tajik@snapp.cab>
2025-02-22 17:35:20 +0330

    Swagger enhancements

--- models/order.go
+++ models/order.go
@@ -6,8 +6,8 @@ const Buy OrderType = "BUY"
 const Sell OrderType = "SELL"
 
 type Order struct {
-	ID string `json:"uuid" binding:"required,uuid4"`
+	ID string `json:"uuid" binding:"required,uuid4" example:"550e8400-e29b-41d4-a716-646655440000"`
 	Action OrderType `json:"action" binding:"required,oneof=BUY SELL"`
-	Price float64 `json:"price" binding:"required"`
-	Amount float64 `json:"amount" binding:"required"`
+	Price float64 `json:"price" binding:"required" example:"100.0"`
+	Amount float64 `json:"amount" binding:"required" example:"10.0"`
 }
